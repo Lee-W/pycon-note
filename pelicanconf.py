f48c70936a8b94d137f73a7a4a065302a430fb21
Wei Lee <weilee.rx@gmail.com>
2020-02-15 15:20:27 +0800

    config: blackify

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,46 +2,42 @@
 from __future__ import unicode_literals
 
 
-PATH = 'content'
+PATH = "content"
 
 # Blog Conf
-AUTHOR = 'Lee-W'
-SITENAME = 'PyCon Note'
-SITEURL = 'http://localhost:8000'
+AUTHOR = "Lee-W"
+SITENAME = "PyCon Note"
+SITEURL = "http://localhost:8000"
 SITETITLE = AUTHOR
 SITELOGO = None
-BROWSER_COLOR = '#333333'
+BROWSER_COLOR = "#333333"
 
 # Locale
-TIMEZONE = 'Asia/Taipei'
-DEFAULT_LANG = 'zh-tw'
-DEFAULT_DATE_FORMAT = '%Y/%m/%d - %a'
+TIMEZONE = "Asia/Taipei"
+DEFAULT_LANG = "zh-tw"
+DEFAULT_DATE_FORMAT = "%Y/%m/%d - %a"
 
 # Page Setting
 MAIN_MENU = True
 DEFAULT_PAGINATION = 10
 MENUITEMS = (
-    ('About', '/about.html'),
-    ('Archives', '/archives.html'),
-    ('Categories', '/categories.html'),
+    ("About", "/about.html"),
+    ("Archives", "/archives.html"),
+    ("Categories", "/categories.html"),
 )
-DIRECT_TEMPLATES = ('index', 'categories', 'tags', 'authors', 'archives', 'search')
+DIRECT_TEMPLATES = ("index", "categories", "tags", "authors", "archives", "search")
 
 # Content Setting
-DEFAULT_CATEGORY = 'Article'
-ARTICLE_URL = 'posts/{category}/{date:%Y}/{date:%m}/{slug}'
-ARTICLE_SAVE_AS = 'posts/{category}/{date:%Y}/{date:%m}/{slug}/index.html'
-STATIC_PATHS = ['images', 'static']
+DEFAULT_CATEGORY = "Article"
+ARTICLE_URL = "posts/{category}/{date:%Y}/{date:%m}/{slug}"
+ARTICLE_SAVE_AS = "posts/{category}/{date:%Y}/{date:%m}/{slug}/index.html"
+STATIC_PATHS = ["images", "static"]
 
 # Theme Setting
-THEME = 'theme/elegant'
-JINJA_ENVIRONMENT = {
-    'extensions': ['jinja2.ext.i18n'],
-}
-EXTRA_PATH_METADATA = {
-    'images': {'path': 'images'},
-}
-PYGMENTS_STYLE = 'default'
+THEME = "theme/elegant"
+JINJA_ENVIRONMENT = {"extensions": ["jinja2.ext.i18n"]}
+EXTRA_PATH_METADATA = {"images": {"path": "images/"}}
+PYGMENTS_STYLE = "default"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -51,29 +47,37 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Social widget
-SOCIAL = (('Linkedin', 'https://tw.linkedin.com/in/clleew'),
-          ('GitHub', 'https://github.com/Lee-W'),
-          ('Gitlab', 'https://gitlab.com/Lee-W'),
-          ('Twitter', 'https://twitter.com/clleew'),
-          ('RSS', '//lee-w.github.io/feeds/all.atom.xml'),)
+SOCIAL = (
+    ("Linkedin", "https://tw.linkedin.com/in/clleew"),
+    ("GitHub", "https://github.com/Lee-W"),
+    ("Gitlab", "https://gitlab.com/Lee-W"),
+    ("Twitter", "https://twitter.com/clleew"),
+    ("RSS", "//lee-w.github.io/feeds/all.atom.xml"),
+)
 
 # Markdown extension
 MARKDOWN = {
-    'extension_configs': {
-        'markdown.extensions.extra': {},
-        'markdown.extensions.codehilite': {'css_class': 'highlight'},
-        'markdown.extensions.nl2br': {},
-        'markdown.extensions.toc': {'toc_depth': '1-3'},
-        'mdx_del_ins': {},
+    "extension_configs": {
+        "markdown.extensions.extra": {},
+        "markdown.extensions.codehilite": {"css_class": "highlight"},
+        "markdown.extensions.nl2br": {},
+        "markdown.extensions.toc": {"toc_depth": "1-3"},
+        "mdx_del_ins": {},
     },
-    'output_format': 'html5'
+    "output_format": "html5",
 }
 
 # Plugin-setting
-PLUGIN_PATHS = ['pelican-plugins']
+PLUGIN_PATHS = ["pelican-plugins"]
 PLUGINS = [
-    'another_read_more_link', 'render_math',
-    'neighbors', 'share_post', 'i18n_subsites', 'tipue_search', 'tag_cloud',
-    'extract_toc', 'post_stats'
+    "another_read_more_link",
+    "render_math",
+    "neighbors",
+    "share_post",
+    "i18n_subsites",
+    "tipue_search",
+    "tag_cloud",
+    "extract_toc",
+    "post_stats",
 ]
-ANOTHER_READ_MORE_LINK = ''
+ANOTHER_READ_MORE_LINK = ""
--- tasks.py
+++ tasks.py
@@ -11,42 +11,42 @@ from pelican.server import ComplexHTTPRequestHandler, RootedHTTPServer
 
 CONFIG = {
     # Local path configuration (can be absolute or relative to tasks.py)
-    'deploy_path': 'output',
+    "deploy_path": "output",
     # Github Pages configuration
-    'github_pages_branch': 'gh-pages',
-    'commit_message': "'Publish site on {}'".format(datetime.date.today().isoformat()),
+    "github_pages_branch": "gh-pages",
+    "commit_message": "'Publish site on {}'".format(datetime.date.today().isoformat()),
     # Port for `serve`
-    'port': 8000,
+    "port": 8000,
 }
 
 
-PELICAN_CMD = 'pipenv run pelican'
+PELICAN_CMD = "pipenv run pelican"
 
 
 @task
 def clean(c):
     """Remove generated files"""
-    if os.path.isdir(CONFIG['deploy_path']):
-        shutil.rmtree(CONFIG['deploy_path'])
-        os.makedirs(CONFIG['deploy_path'])
+    if os.path.isdir(CONFIG["deploy_path"]):
+        shutil.rmtree(CONFIG["deploy_path"])
+        os.makedirs(CONFIG["deploy_path"])
 
 
 @task
 def build(c):
     """Build local version of site"""
-    c.run(f'{PELICAN_CMD} -s pelicanconf.py')
+    c.run(f"{PELICAN_CMD} -s pelicanconf.py")
 
 
 @task
 def rebuild(c):
     """`build` with the delete switch"""
-    c.run(f'{PELICAN_CMD} -d -s pelicanconf.py')
+    c.run(f"{PELICAN_CMD} -d -s pelicanconf.py")
 
 
 @task
 def regenerate(c):
     """Automatically regenerate site upon file modification"""
-    c.run(f'{PELICAN_CMD} -r -s pelicanconf.py', pty=True)
+    c.run(f"{PELICAN_CMD} -r -s pelicanconf.py", pty=True)
 
 
 @task
@@ -57,12 +57,10 @@ def serve(c):
         allow_reuse_address = True
 
     server = AddressReuseTCPServer(
-        CONFIG['deploy_path'],
-        ('', CONFIG['port']),
-        ComplexHTTPRequestHandler
+        CONFIG["deploy_path"], ("", CONFIG["port"]), ComplexHTTPRequestHandler
     )
 
-    sys.stderr.write('Serving on port {port} ...\n'.format(**CONFIG))
+    sys.stderr.write("Serving on port {port} ...\n".format(**CONFIG))
     server.serve_forever()
 
 
@@ -76,19 +74,21 @@ def reserve(c):
 @task
 def preview(c):
     """Build production version of site"""
-    c.run(f'{PELICAN_CMD} -s publishconf.py')
+    c.run(f"{PELICAN_CMD} -s publishconf.py")
 
 
 @task
 def publish(c):
     """Publish to production via rsync"""
-    c.run(f'{PELICAN_CMD} -s publishconf.py')
+    c.run(f"{PELICAN_CMD} -s publishconf.py")
 
 
 @task
 def github(c):
     """Publish to GitHub Pages"""
     preview(c)
-    c.run('pipenv run ghp-import -b {github_pages_branch} '
-          '-m {commit_message} '
-          '{deploy_path} -p'.format(**CONFIG))
+    c.run(
+        "pipenv run ghp-import -b {github_pages_branch} "
+        "-m {commit_message} "
+        "{deploy_path} -p".format(**CONFIG)
+    )
